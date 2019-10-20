7f999fe94aac5ba168bbc7726bbb4d9c18ebbd96
inequihi <inequihillalt@yahoo.com.ar>
2019-10-20 15:39:07 -0300

    Corrección TP9_Ej_3.c
    a. El objetivo de la función es sumar los dígitos de un numero entero (signado o no signado)
    b. No funciona por 2 cosas:
         1. No se llama a la función suma_digitos (se llama a digitos).
         2. La funcion recursiva suma_digitos no plantea un caso base para terminar de analizar digitos. 
    
    Se arreglaron esos errores y además se agregó una conidición para poder sumar dígitos de numeros negativos

--- TP9_Ej_3.c
+++ TP9_Ej_3.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 
-int suma_digitos(int n1);
+int suma_digitos(int n1);    //Prototipo de funcion recursiva para sumar los digitos de un numero
 
 int main (void){
   int num = 109;
-
-  int num2 = suma_digitos(num);
-
-  printf("%u \n %u \n ", num, num2);
+  int num_neg = -109;
+  
+  int sum_num = suma_digitos(num);         //Llamamos a funcion recursiva
+  int sum_num_neg = suma_digitos(num_neg);
+  
+  printf("%d \n %d \n", num, sum_num);      //Imprmimimos para verificar resultado
+  printf("%d \n %d \n", num_neg, sum_num_neg);      //Imprmimimos para verificar resultado
   return 0;
 }
 
 
-int suma_digitos(int n1)
-{
- return ((n1 % 10) + digitos(n1 / 10)); //caso recursivo
+int suma_digitos(int n1){
+  if(n1 < 0){                   //En caso de querer sumar los digitos de un numero negativo
+    n1 *=-1;                        //lo convertimos a positivo
+  }
+  else if(n1<1){
+    return 1;                 //Caso base: No hay mas digitos que sumar
+  }
+ return ((n1 % 10) + suma_digitos(n1 / 10)); //Caso recursivo
 }
